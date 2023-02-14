3923e4119d9f49e1ede6097b7e96ec67bd071149
Sascha-davidson <saschadavidson1@gmail.com>
2023-02-14 16:17:47 +0100

    het begin van mijn werk

---
+++ public/script.js
@@ -0,0 +1,8 @@
+// let card = document.querySelector(".flip-card");
+// let flip_card_inner = document.querySelector(".flip-card-inner");
+// let flip_card_back = document.querySelector(".flip-card-back");
+
+// card.addEventListener("click", () => {
+//   flip_card_inner.classList.toggle("flip");
+//   flip_card_back.classList.toggle("flip");
+// });
