59696bc71eddd34eb764ae892689d59c5d7b3047
Sascha-davidson <saschadavidson1@gmail.com>
2023-02-20 12:26:10 +0100

    adding a card

--- public/script.js
+++ public/script.js
@@ -1,33 +1,12 @@
-let canvas = document.querySelector('.canvas')
-let context = canvas.getContext("2d");
+const allTriggers = document.querySelectorAll('.card-trigger')
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+console.log(allTriggers);
 
-context.fillStyle = "green";
-context.fillRect(10, 10, 150, 100);
-
-const scene = engine.createScene();
-
-const a = new Circle(new Vector2(innerWidth / 2, innerHeight / 2), 125, "green", false);
-const b = new Circle(new Vector2(innerWidth / 3, innerHeight / 2), 125, "red", false)
-
-a.velocity = new Vector2(-5,5);
-b.velocity = new Vector2(5,4);
-
-let background = new Cube(new Vector2(0,0),window.innerWidth, window.innerHeight);
-background.color = "black";
-
-
-scene.addGameObjects(a, b, background)
-
-scene.OnUpdate.AddListener(update);
-
-function update() {
-    ConfineInScreen.ConfineCircleInScreen(a)
-    ConfineInScreen.ConfineCircleInScreen(b)
-
-    if (a.position.distanceTo(b.position) < a.radius + b.radius) {
-        Circle.resolveCollision(a,b);
-    }
-}
+allTriggers.forEach((trigger) => {
+    console.log(trigger)
+    trigger.addEventListener('click', () => {
+        const dataId = trigger.dataset.id
+        const card = document.getElementById(dataId)
+        card.classList.add('active')
+    })
+})
